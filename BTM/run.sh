54625ca764459f0a153bf8b4241a9e0f97aafea4
feelsbadman <abcdegtxx.testing@gmail.com>
2016-12-27 12:12:53 +0800

    mv src to BTM folder

---
+++ BTM/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+K=$1
+alpha=`echo "scale=3;50/$K"|bc`
+beta=0.005
+niter=5
+save_step=501
+
+streamer=$2
+output_dir=./streamer/${streamer}/output/
+model_dir=${output_dir}model/
+dwid_pt=${output_dir}doc_wids.txt
+voca_pt=${output_dir}vocabulary.txt
+
+echo ""
+echo "=============== Topic Learning ============="
+W=`wc -l < $voca_pt` # vocabulary size
+echo "run ./BTM/btm est $K $W $alpha $beta $niter $save_step $dwid_pt $model_dir"
+./src/btm est $K $W $alpha $beta $niter $save_step $dwid_pt $model_dir
+
+
+# infer p(z|d) for each doc
+echo ""
+echo "================ Infer P(z|d)==============="
+echo "run ./BTM/btm inf sum_b $K $dwid_pt $model_dir"
+./BTM/src/btm inf sum_b $K $dwid_pt $model_dir
+
+
